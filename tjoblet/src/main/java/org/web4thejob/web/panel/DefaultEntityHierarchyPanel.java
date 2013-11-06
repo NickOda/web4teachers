0f64f08dd502059f09058d5c54b8005ad01af8bc
bissaias <bissaias@hotmail.com>
2013-11-06 12:40:06 +0200

    Category Tree

--- tjoblet/src/main/java/org/web4thejob/web/panel/DefaultEntityHierarchyPanel.java
+++ tjoblet/src/main/java/org/web4thejob/web/panel/DefaultEntityHierarchyPanel.java
@@ -1,18 +1,33 @@
 package org.web4thejob.web.panel;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.orm.hibernate4.HibernateOptimisticLockingFailureException;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.web4thejob.SystemProtectedEntryException;
 import org.web4thejob.command.Command;
 import org.web4thejob.command.CommandEnum;
 import org.web4thejob.context.ContextUtil;
+import org.web4thejob.message.Message;
+import org.web4thejob.message.MessageArgEnum;
+import org.web4thejob.message.MessageEnum;
+import org.web4thejob.message.MessageListener;
+import org.web4thejob.orm.Entity;
+import org.web4thejob.setting.Setting;
+import org.web4thejob.setting.SettingEnum;
 import org.web4thejob.tjoblet.orm.EntityHierarchy;
 import org.web4thejob.tjoblet.orm.EntityHierarchyItem;
+import org.web4thejob.util.L10nMessages;
+import org.web4thejob.util.L10nString;
+import org.web4thejob.web.dialog.EntityPersisterDialog;
+import org.web4thejob.web.panel.base.zk.AbstractZkBindablePanel;
 import org.web4thejob.web.panel.base.zk.AbstractZkTargetTypeAwarePanel;
 import org.web4thejob.web.util.ZkUtil;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Components;
 import org.zkoss.zk.ui.event.*;
 import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Tree;
 import org.zkoss.zul.Treechildren;
 import org.zkoss.zul.Treeitem;
@@ -27,6 +42,10 @@ import java.util.*;
 @Scope("prototype")
 public class DefaultEntityHierarchyPanel extends AbstractZkTargetTypeAwarePanel implements EntityHierarchyPanel,
         EventListener<Event> {
+    public static final L10nString L10N_MSG_CANNOT_DELETE_PARENT = new L10nString(EntityHierarchyPanel.class,
+            "message_cannot_delete_parent", "This item cannot be deleted because it has child items. Remove child " +
+            "items and retry.");
+
     private static final String ATTRIB_HIERARCHY = "ATTRIB_HIERARCHY";
     private static final String ATTRIB_ITEM = "ATTRIB_ITEM";
     private static final String ON_OPEN_ECHO = Events.ON_OPEN + "Echo";
@@ -139,8 +158,11 @@ public class DefaultEntityHierarchyPanel extends AbstractZkTargetTypeAwarePanel 
             boolean selected = tree.getSelectedItem() != null;
             getCommand(CommandEnum.MOVE_UP).setActivated(selected);
             getCommand(CommandEnum.MOVE_DOWN).setActivated(selected);
-            getCommand(CommandEnum.ADD).setActivated(selected);
             getCommand(CommandEnum.REMOVE).setActivated(selected);
+        }
+        if (hasCommand(CommandEnum.REFRESH)) {
+            getCommand(CommandEnum.REFRESH).setActivated(hasTargetType());
+            getCommand(CommandEnum.ADD).setActivated(hasTargetType());
         }
     }
 
@@ -167,7 +189,8 @@ public class DefaultEntityHierarchyPanel extends AbstractZkTargetTypeAwarePanel 
             Treeitem dragged = ((Treeitem) dropEvent.getDragged());
             Treechildren dragged_source = (Treechildren) dragged.getParent();
 
-            if (dragged.getParent() != null && dragged.getParent().equals(target.getTreechildren())) {
+            if (Components.isAncestor(dragged, target) ||
+                    (dragged.getParent() != null && dragged.getParent().equals(target.getTreechildren()))) {
                 return;
             }
 
@@ -189,6 +212,7 @@ public class DefaultEntityHierarchyPanel extends AbstractZkTargetTypeAwarePanel 
                 eh.setSorting(target.getTreechildren().getItemCount() + 1);
                 ContextUtil.getDWS().save(eh);
 
+                dragged.setParent(null);
                 dragged.setParent(target.getTreechildren());
                 dragged.setAttribute(ATTRIB_HIERARCHY, eh);
                 dragged.setSelected(true);
@@ -213,38 +237,138 @@ public class DefaultEntityHierarchyPanel extends AbstractZkTargetTypeAwarePanel 
                 Treeitem item = tree.getSelectedItem();
                 Treeitem sibling = (Treeitem) item.getPreviousSibling();
                 swap(sibling, item);
+                tree.clearSelection();
                 tree.setSelectedItem(item);
             }
         } else if (CommandEnum.MOVE_DOWN.equals(command.getId())) {
             if (tree.getSelectedItem() != null) {
                 Treeitem item = tree.getSelectedItem();
                 Treeitem sibling = (Treeitem) item.getNextSibling();
-                Treeitem newItem = swap(item, sibling);
-                if (newItem != null) {
-                    tree.setSelectedItem(newItem);
-                }
+                swap(item, sibling);
+                tree.clearSelection();
+                tree.setSelectedItem(item);
             }
         } else if (CommandEnum.REMOVE.equals(command.getId())) {
             if (tree.getSelectedItem() != null) {
-                Treeitem item = tree.getSelectedItem();
-                EntityHierarchy eh = ((EntityHierarchy) item.getAttribute(ATTRIB_HIERARCHY));
+                final Treeitem item = tree.getSelectedItem();
+                final EntityHierarchyItem ehi = ((EntityHierarchyItem) item.getAttribute(ATTRIB_ITEM));
+                final EntityHierarchy eh = ((EntityHierarchy) item.getAttribute(ATTRIB_HIERARCHY));
                 if (eh != null) {
                     ContextUtil.getDWS().delete(eh);
-                    item.setParent(null);
-                    renderRootItem();
+                    if (item.getParent().getChildren().size() == 1) {
+                        item.getParent().detach();
+                    }
+                    item.setParent(tree.getTreechildren());
+                    item.removeAttribute(ATTRIB_HIERARCHY);
+                    tree.clearSelection();
+                    tree.setSelectedItem(item);
+                } else if (ehi != null) {
+                    if (item.getTreechildren() != null && item.getTreechildren().getItemCount() > 0) {
+                        displayMessage(L10N_MSG_CANNOT_DELETE_PARENT.toString(), false);
+                        return;
+                    }
+
+                    Messagebox.show(AbstractZkBindablePanel.L10N_MSG_DELETE_CONFIRMATION.toString(),
+                            L10nMessages.L10N_MSGBOX_TITLE_QUESTION
+                                    .toString(), new Messagebox.Button[]{Messagebox.Button.OK,
+                            Messagebox.Button.CANCEL},
+                            null, Messagebox.QUESTION, Messagebox.Button.CANCEL,
+                            new EventListener<Messagebox.ClickEvent>() {
+                                @Override
+                                public void onEvent(Messagebox.ClickEvent event) throws Exception {
+                                    if (Messagebox.Button.OK == event.getButton()) {
+                                        Message message = ContextUtil.getMessage(MessageEnum.ENTITY_DELETED,
+                                                this,
+                                                MessageArgEnum.ARG_ITEM, ehi);
+                                        try {
+                                            ContextUtil.getDWS().delete(ehi);
+                                            item.detach();
+                                            dispatchMessage(message);
+                                        } catch (HibernateOptimisticLockingFailureException e) {
+                                            displayMessage(AbstractZkBindablePanel
+                                                    .L10N_MSG_ENTITY_MODIFIED_BY_OTHERS.toString(), true);
+                                        } catch (SystemProtectedEntryException e) {
+                                            displayMessage(e.getMessage(), true);
+                                        } catch (Exception e) {
+                                            displayMessage(AbstractZkBindablePanel.L10N_MSG_DELETION_FAILED
+                                                    .toString(), true);
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+                            });
                 }
             }
+        } else if (CommandEnum.ADD.equals(command.getId())) {
+            EntityHierarchyItem templEntity;
+            try {
+                templEntity = (EntityHierarchyItem) entityHierarchy.getItemsType().newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return;
+            }
+            Set<Setting<?>> settings = new HashSet<Setting<?>>(1);
+            settings.add(ContextUtil.getSetting(SettingEnum.TARGET_TYPE, entityHierarchy.getItemsType()));
+            EntityPersisterDialog dialog = ContextUtil.getDefaultDialog(EntityPersisterDialog.class,
+                    templEntity, settings, MutableMode.INSERT, true);
+            dialog.setL10nMode(getL10nMode());
+            dialog.show(new MessageListener() {
+                @Override
+                public void processMessage(Message message) {
+                    if (MessageEnum.AFFIRMATIVE_RESPONSE == message.getId()) {
+                        List<Entity> toSave = new ArrayList<Entity>(2);
+                        EntityHierarchyItem child = message.getArg(MessageArgEnum.ARG_ITEM, EntityHierarchyItem.class);
+                        toSave.add(child);
+
+                        Treeitem item = tree.getSelectedItem();
+                        EntityHierarchy hierarchy = null;
+                        if (item != null && item.getAttribute(ATTRIB_ITEM) instanceof EntityHierarchyItem) {
+                            if (item.getTreechildren() == null) {
+                                new Treechildren().setParent(item);
+                            }
+                            EntityHierarchyItem parent = (EntityHierarchyItem) item.getAttribute(ATTRIB_ITEM);
+
+                            try {
+                                hierarchy = (EntityHierarchy) getTargetType().newInstance();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                return;
+                            }
+
+                            hierarchy.setParent(parent);
+                            hierarchy.setChild(child);
+                            hierarchy.setSorting(item.getTreechildren().getItemCount() + 1);
+                            toSave.add(hierarchy);
+                        }
+
+                        ContextUtil.getDWS().save(toSave);
+
+                        Treeitem newItem = new Treeitem();
+                        newItem.setLabel(child.toRichString());
+                        newItem.setAttribute(ATTRIB_ITEM, child);
+
+                        if (hierarchy != null) {
+                            newItem.setParent(item.getTreechildren());
+                            newItem.setAttribute(ATTRIB_HIERARCHY, hierarchy);
+                        } else {
+                            newItem.setParent(tree.getTreechildren());
+                        }
+                    }
+                }
+            });
+
+
         }
         super.processValidCommand(command);
     }
 
-    private Treeitem swap(Treeitem item1, Treeitem item2) {
+    private void swap(Treeitem item1, Treeitem item2) {
         if (item1 != null && item2 != null) {
 
             EntityHierarchy eh1 = ((EntityHierarchy) item1.getAttribute(ATTRIB_HIERARCHY));
             EntityHierarchy eh2 = ((EntityHierarchy) item2.getAttribute(ATTRIB_HIERARCHY));
 
-            if (eh1 == null || eh2 == null) return null;
+            if (eh1 == null || eh2 == null) return;
 
             long tmp = eh1.getSorting();
             eh1.setSorting(eh2.getSorting());
@@ -255,9 +379,7 @@ public class DefaultEntityHierarchyPanel extends AbstractZkTargetTypeAwarePanel 
 
             item2.detach();
             item1.getParentItem().getTreechildren().insertBefore(item2, item1);
-            return item2;
         }
-        return null;
     }
 
 }
